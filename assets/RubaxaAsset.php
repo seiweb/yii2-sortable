0bf03bdd3c2d680de929364c204c03b971e3f756
seiweb <e.stasenko@gmail.com>
2018-12-04 19:05:53 +0300

    Update RubaxaAsset.php

--- assets/RubaxaAsset.php
+++ assets/RubaxaAsset.php
@@ -9,7 +9,8 @@ class RubaxaAsset extends AssetBundle
     public $sourcePath = '@vendor/seiweb/yii2-sortable/assets/files';
 
     public $js = [
-        '//rubaxa.github.io/Sortable/Sortable.js',
+        //'//rubaxa.github.io/Sortable/Sortable.js',
+        'js/sortable.js',
         'js/jquery.binding.js',
     ];
 
